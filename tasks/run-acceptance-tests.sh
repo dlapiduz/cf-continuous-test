f383946b35055c6f203a41ef1c7f504ce85587a9
Diego Lapiduz <dlapiduz@pivotal.io>
2017-04-04 16:47:23 -0500

    Run acceptance tests too

---
+++ tasks/run-acceptance-tests.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+set -o pipefail
+
+echo "Interpolating config..."
+export CONFIG=$(pwd)/pipeline/tasks/cats_config.json
+
+export JOB_PWD=$(pwd)
+
+# Replace all variables with parameters from concourse
+# Parameters are received as environment variables
+sed -i 's,\%API_URI\%,'"${API_URI}"',g' $CONFIG
+sed -i 's,\%APPS_DOMAIN\%,'"${APPS_DOMAIN}"',g' $CONFIG
+sed -i 's,\%CF_USER\%,'"${CF_USER}"',g' $CONFIG
+sed -i 's,\%CF_PASSOWRD\%,'"${CF_PASSOWRD}"',g' $CONFIG
+
+echo "Installing packages..."
+apk add --update curl bash git openssh
+curl -L 'https://cli.run.pivotal.io/stable?release=linux64-binary' | tar -zx -C /usr/local/bin
+
+# Link to gopath
+mkdir -p /go/src/github.com/cloudfoundry
+ln -s `pwd`/cf-acceptance-tests /go/src/github.com/cloudfoundry/cf-acceptance-tests
+
+# Run tests
+echo "Setup tests..."
+cd /go/src/github.com/cloudfoundry/cf-acceptance-tests
+
+go get -d github.com/cloudfoundry/cf-acceptance-tests || true
+./bin/update_submodules
+
+echo "Run tests..."
+./bin/test -nodes=2 | tee $JOB_PWD/run-stats/build-output.txt
+
+tail -n 3 $JOB_PWD/run-stats/build-output.txt | grep "Ginkgo ran" | rev | cut -d' ' -f 1 | rev > $JOB_PWD/run-stats/acceptance-build-time-$(date +%s).txt
--- tasks/run-smoke-tests.sh
+++ tasks/run-smoke-tests.sh
@@ -5,7 +5,7 @@ set -e
 set -o pipefail
 
 echo "Interpolating config..."
-export CONFIG=$(pwd)/pipeline/tasks/integration_config.json
+export CONFIG=$(pwd)/pipeline/tasks/smoke_config.json
 
 export JOB_PWD=$(pwd)
 
